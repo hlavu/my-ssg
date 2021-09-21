6905217331ce8ba51f75d2c8e7614b2b9443be16
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-09-20 21:48:06 -0400

    Format code using VS Code 'Prettier' extension for better readability

--- bin/modules/readFolder.js
+++ bin/modules/readFolder.js
@@ -5,34 +5,34 @@ const path = require("path");
 var body = "";
 
 module.exports.readFolder = function (inputPath, cssLink, outputContainer) {
-  fs.readdir(inputPath, function (err, files) {
-    if (err) {
-      return console.log(err);
-    }
+    fs.readdir(inputPath, function (err, files) {
+        if (err) {
+            return console.log(err);
+        }
 
-    var sortedFile = files.filter(
-      (file) => path.extname(`${inputPath}/${file}`) === ".txt"
-    );
-    
-    sortedFile.forEach(function (file) {
-      var fileName = fileModule.readFile(
-        `${inputPath}/${file}`,
-        cssLink,
-        outputContainer
-      );
+        var sortedFile = files.filter(
+            (file) => path.extname(`${inputPath}/${file}`) === ".txt"
+        );
 
-      var url = `./${encodeURI(fileName)}.html`;
+        sortedFile.forEach(function (file) {
+            var fileName = fileModule.readFile(
+                `${inputPath}/${file}`,
+                cssLink,
+                outputContainer
+            );
 
-      // index.html body
-      body += `<h5><a href=\"${url}\">${fileName}</h5>\n`;
+            var url = `./${encodeURI(fileName)}.html`;
+
+            // index.html body
+            body += `<h5><a href=\"${url}\">${fileName}</h5>\n`;
+        });
+
+        // create index.html
+        html.generateHTML(
+            "index",
+            cssLink,
+            `<h4>Generated Sites</h4>\n${body}`,
+            outputContainer
+        );
     });
-
-    // create index.html
-    html.generateHTML(
-      "index",
-      cssLink,
-      `<h4>Generated Sites</h4>\n${body}`,
-      outputContainer
-    );
-  });
 };
